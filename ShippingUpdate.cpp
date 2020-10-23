5d09dc73f377a5c7198652bfeb7f444e2c499d52
cezpeleta1 <54698706+cezpeleta1@users.noreply.github.com>
2020-10-23 01:59:24 -0700

    Update ShippingUpdate.cpp

--- ShippingUpdate.cpp
+++ ShippingUpdate.cpp
@@ -14,15 +14,18 @@ namespace PackageTracking {
   ShippingUpdate::ShippingUpdate(const std::string& description,
 				 const std::string& location,
 				 std::time_t timestamp) noexcept {
-    // TODO: Implement this function.
-    // Before submitting your assignment, delete all TODO comments
-    // including this one.
+    description_ = description;
+    location_ = location;
+    timestamp_ = timestamp;
+    //prev = setPrev;
+  //  next = setNext;
+  
   }
 
   const std::string& ShippingUpdate::Description() const noexcept {
     return description_;
   }
-  
+
   const std::string& ShippingUpdate::Location() const noexcept {
     return location_;
   }
@@ -30,12 +33,12 @@ namespace PackageTracking {
   time_t ShippingUpdate::Timestamp() const noexcept {
     return timestamp_;
   }
-  
+
   std::string ShippingUpdate::Describe() const noexcept {
-    // TODO: Implement this function, including the return statement.
-    // Before submitting your assignment, delete all TODO comments
-    // including this one.
-    return "";
+    std::string sstream;
+    sstream = timestamp_;
+    std::string returnstring = sstream + " " + description_ + " " + location_ + "\n";
+     return returnstring;
   }
-  
+
 }
