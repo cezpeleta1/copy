59d11bebbaf1cb97534f62e5c97b754e37556c54
cezpeleta1 <54698706+cezpeleta1@users.noreply.github.com>
2020-10-23 01:58:38 -0700

    Update PackageStatus.cpp

--- PackageStatus.cpp
+++ PackageStatus.cpp
@@ -1,4 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // PackageStatus.cpp
 //
 // class PackageStatus
@@ -13,45 +13,82 @@
 namespace PackageTracking {
 
   PackageStatus::PackageStatus() noexcept {
-    // TODO: Depending on the data members you choose, you may need to
-    // add code to this constructor.
-    // Before submitting your assignment, delete all TODO comments
-    // including this one.
+
+    Update = 0;
+    heading = new PackageStatus;
+    ending = new PackageStatus;
+
+
+     heading->setNext(heading);
+     ending-> setPrev(ending);
+
+    iterator = heading;
+    int strnum=0;
+    tracking_number_ = strnum ;
+
   }
-  
+
   PackageStatus::PackageStatus(const std::string& tracking_number) noexcept {
-    // TODO: Implement this function.
-    // Before submitting your assignment, delete all TODO comments
-    // including this one.
+    if(!Empty()) {
+
+
+      delete heading;
+      delete ending;
+    }
   }
 
   const std::string& PackageStatus::TrackingNumber() const noexcept {
     return tracking_number_;
   }
-  
+
   int PackageStatus::Size() const noexcept {
     // TODO: Implement this function, including the return statement.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
+    
     return 0;
   }
 
   bool PackageStatus::Empty() const noexcept {
-    // TODO: Implement this function, including the return statement.
-    // Before submitting your assignment, delete all TODO comments
-    // including this one.
+    if (heading -> getNext() == ending)
+      return true;
     return false;
   }
 
   void PackageStatus::AddUpdate(const std::string& description,
 				const std::string& location,
-				std::time_t timestamp) {
+				std::time_t timestamp){
+
+      ShippingUpdate*ptr_newupdate = new ShippingUpdate(?)
+      if(!ptr_newupdate)
+            return-1;
+
+      ptr_newupdate-> setNext(ending);
+
+      ShippingUpdate *ptr_preending = heading ->getPrev();
+      ptr_newupdate-> setPrev(ptr_preending);
+      ptr_preending->setNext(ptr_newupdate);
+      ending-> setPrev(ptr_newupdate);
+
+      Update++;
+
+      iterator = ptr_newupdate;
+
+      return 1; --------??
     // TODO: Implement this function.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
   }
 
   bool PackageStatus::MoveCursorBackward() noexcept {
+
+    ShippingUpdate* ptr_iteratorpre = iterator->getPrev();
+    if(ptr_iteratorpre == heading)
+    return -1;
+
+    iterator = ptr_iteratorpre;
+    return 1; -----??
+
     // TODO: Implement this function, including the return statement.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
@@ -59,6 +96,14 @@ namespace PackageTracking {
   }
 
   bool PackageStatus::MoveCursorForward() noexcept {
+
+    ShippingUpdate* ptr_iteratornext = iterator-> getNext();
+    if(ptr_iteratornext == ending)
+      return -1;
+
+    iterator = ptr_iteratornext;
+    return 1; ---??
+
     // TODO: Implement this function, including the return statement.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
@@ -77,9 +122,23 @@ namespace PackageTracking {
     // Before submitting your assignment, delete all TODO comments
     // including this one.
     return "";
+    ]""
   }
 
   std::string PackageStatus::DescribePreviousUpdates() {
+    if(Empty() || iterator->getPrev() == heading)
+    {
+      cout << "No previous Updates" << endl;
+      return -1;
+    }
+
+    ShippingUpdate* iterator = heading;
+    while(heading->getPrev()!=header)
+    {
+      MoveCursorBackward;
+      cout << //gettime() << ":" << getStatus 90 << ";" << GETlocation90 ,, ENDL;
+     }
+     iterator = ptr_iteraaator;
     // TODO: Implement this function, including the return statement.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
@@ -87,6 +146,22 @@ namespace PackageTracking {
   }
 
   std::string PackageStatus::DescribeFollowingUpdates() {
+    //cout<<m_getTime()<<": "<<m_getStatus()<<": "<<m_getLocation()<<endl;
+    if (Empty() || iterator->getNext() == heading)
+    {
+      cout << "No Following Updates" << endl;
+      return -1;
+    }
+    ShippingUpdate* ptr_iteraaator = iterator;
+    while (iterator-> getNext()! == ending)
+    {
+      MoveCursorForward;
+      //cout<<m_getTime()<<": "<<m_getStatus()<<": "<<m_getLocation()<<endl;
+    }
+
+    iterator = ptr_iteraaator
+    return 1; ?
+
     // TODO: Implement this function, including the return statement.
     // Before submitting your assignment, delete all TODO comments
     // including this one.
